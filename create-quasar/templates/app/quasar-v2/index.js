557208e565a2bb7a2c89746e50be6c136c063775
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2024-11-08 15:23:09 +0200

    feat(create-quasar): mention vite 6

--- create-quasar/templates/app/quasar-v2/index.js
+++ create-quasar/templates/app/quasar-v2/index.js
@@ -7,7 +7,7 @@ export async function script ({ scope, utils }) {
       initial: 0,
       choices: [
         { title: 'Quasar App CLI with Vite 2 (stable | v1)', value: 'vite-1', description: 'recommended' },
-        { title: 'Quasar App CLI with Vite 5 (BETA | next major version - v2)', value: 'vite-2' },
+        { title: 'Quasar App CLI with Vite 6 (BETA | next major version - v2)', value: 'vite-2' },
         { title: 'Quasar App CLI with Webpack (stable | v3)', value: 'webpack-3' },
         { title: 'Quasar App CLI with Webpack (BETA | next major version - v4)', value: 'webpack-4' }
       ]
