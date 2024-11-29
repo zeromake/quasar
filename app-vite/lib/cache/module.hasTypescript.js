c754c959e1c678fc002266644635c2a3dc410ab7
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2024-11-29 16:49:30 +0200

    feat+perf(app-vite/app-webpack): relax TS detection even more (only /tsconfig.json is needed)

--- app-vite/lib/cache/module.hasTypescript.js
+++ app-vite/lib/cache/module.hasTypescript.js
@@ -1,18 +1,5 @@
 import { existsSync } from 'node:fs'
 
-import { getPackagePath } from '../utils/get-package-path.js'
-
 export function createInstance ({ appPaths }) {
-  const hasTypescript = (
-    existsSync(appPaths.resolve.app('tsconfig.json'))
-  )
-
-  if (hasTypescript === true) {
-    const typescriptPath = getPackagePath('typescript', appPaths.appDir)
-    if (typescriptPath === void 0) {
-      return false
-    }
-  }
-
-  return hasTypescript
+  return existsSync(appPaths.resolve.app('tsconfig.json'))
 }
--- app-webpack/lib/cache/module.hasTypescript.js
+++ app-webpack/lib/cache/module.hasTypescript.js
@@ -1,23 +1,5 @@
 const { existsSync } = require('node:fs')
 
-const { getPackagePath } = require('../utils/get-package-path.js')
-
 module.exports.createInstance = function createInstance ({ appPaths }) {
-  const hasTypescript = (
-    existsSync(appPaths.resolve.app('tsconfig.json'))
-  )
-
-  if (hasTypescript === true) {
-    const typescriptPath = getPackagePath('typescript', appPaths.appDir)
-    if (typescriptPath === void 0) {
-      return false
-    }
-
-    const tsLoaderPath = getPackagePath('ts-loader', appPaths.appDir)
-    if (tsLoaderPath === void 0) {
-      return false
-    }
-  }
-
-  return hasTypescript
+  return existsSync(appPaths.resolve.app('tsconfig.json'))
 }
